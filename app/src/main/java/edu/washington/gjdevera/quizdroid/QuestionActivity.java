c22893dec30491b520234881ba71de8ef5811456
Jacob Devera <jacob.devera12@gmail.com>
2017-01-28 20:58:33 -0800

    Added question activity

--- app/src/main/java/edu/washington/gjdevera/quizdroid/QuestionActivity.java
+++ app/src/main/java/edu/washington/gjdevera/quizdroid/QuestionActivity.java
@@ -3,19 +3,56 @@ package edu.washington.gjdevera.quizdroid;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class QuestionActivity extends Activity {
+    private int correct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
 
+        correct = 0;
         Intent intent = getIntent();
-        int quizNumber = intent.getIntExtra(TopicOverviewActivity.EXTRA_OVERVIEW, 0);
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
+        // get question number based on previous question number
+        int questionNumber = intent.getIntExtra(TopicOverviewActivity.EXTRA_OVERVIEW, 0);
+        TextView question = (TextView) findViewById(R.id.question);
+        question.setText(getResources().getStringArray(R.array.questions)[questionNumber]);
+        question.setTextSize(40);
 
+        RadioButton radio1 = (RadioButton) findViewById(R.id.radio_1);
+        RadioButton radio2 = (RadioButton) findViewById(R.id.radio_2);
+        RadioButton radio3 = (RadioButton) findViewById(R.id.radio_3);
+        RadioButton radio4 = (RadioButton) findViewById(R.id.radio_4);
+        String[] radioArray = getResources().getStringArray(R.array.choices);
+        radio1.setText(radioArray[0]);
+        radio2.setText(radioArray[1]);
+        radio3.setText(radioArray[2]);
+        radio4.setText(radioArray[3]);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        Button next = (Button) findViewById(R.id.btn_next);
+        next.setVisibility(View.VISIBLE);
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_1:
+                if (checked)
+
+                    break;
+            case R.id.radio_2:
+                if (checked)
+
+                    break;
+        }
     }
 }
--- app/src/main/java/edu/washington/gjdevera/quizdroid/TopicOverviewActivity.java
+++ app/src/main/java/edu/washington/gjdevera/quizdroid/TopicOverviewActivity.java
@@ -9,6 +9,8 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class TopicOverviewActivity extends Activity {
     public final static String EXTRA_OVERVIEW = "com.example.quizdroid.OVERVIEW";
 
@@ -16,24 +18,29 @@ public class TopicOverviewActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topic_overview);
-        String[] descriptions = getResources().getStringArray(R.array.descriptions);
 
         Intent intent = getIntent();
         final int quizNumber = intent.getIntExtra(MainActivity.EXTRA_TOPIC, 0);
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText(descriptions[quizNumber]);
+        TextView descText = new TextView(this);
+        TextView numberText = new TextView(this);
+        descText.setTextSize(20);
+        numberText.setTextSize(20);
+
+        numberText.setText(getString(R.string.number_of_questions));
+        descText.setText(getString(R.string.description));
 
         LinearLayout layout = (LinearLayout) findViewById(R.id.activity_topic_overview);
-        layout.addView(textView, 0);
+        layout.addView(descText, 0);
+        layout.addView(numberText, 1);
 
-        final Button button = (Button) findViewById(R.id.btn_next);
+        final Button button = (Button) findViewById(R.id.btn_begin);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), TopicOverviewActivity.class);
-                intent.putExtra(EXTRA_OVERVIEW, quizNumber);
+                Intent intent = new Intent(getApplicationContext(), QuestionActivity.class);
+                intent.putExtra(EXTRA_OVERVIEW, 0); // 0 = start on first question
                 startActivity(intent);
+                finish();
             }
         });
     }
