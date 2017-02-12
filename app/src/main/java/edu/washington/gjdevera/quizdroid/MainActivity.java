aad5c87d4156cd4bae4adba886050f97522cac9e
Jacob Devera <jacob.devera12@gmail.com>
2017-02-12 01:03:02 -0800

    Changed ListView into a RecyclerView with icons

--- app/src/main/java/edu/washington/gjdevera/quizdroid/MainActivity.java
+++ app/src/main/java/edu/washington/gjdevera/quizdroid/MainActivity.java
@@ -3,20 +3,30 @@ package edu.washington.gjdevera.quizdroid;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_TOPIC = "edu.washington.gjdevera.quizdroid.TOPIC";
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] topics = getResources().getStringArray(R.array.topics);
+        final List<Topic> topics = ((QuizApp) getApplication()).getRepository().getAllTopics();
+
+        /*
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1, topics);
 
@@ -30,5 +40,50 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        */
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(new RecyclerView.Adapter<TopicViewHolder>() {
+
+            @Override
+            public TopicViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View v = LayoutInflater.from(parent.getContext()).inflate(
+                        R.layout.topic_row,
+                        parent,
+                        false);
+                return new TopicViewHolder(v);
+            }
+
+            @Override
+            public void onBindViewHolder(TopicViewHolder vh, int position) {
+                TextView tv = (TextView) vh.itemView.findViewById(R.id.text_id);
+                tv.setText(topics.get(position).getTitle());
+                tv.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_library_books, 0, 0, 0);
+                tv.setCompoundDrawablePadding(12);
+            }
+
+            @Override
+            public int getItemCount() {
+                return topics.size();
+            }
+        });
+    }
+
+    private class TopicViewHolder
+            extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
+
+        public TopicViewHolder(View v) {
+            super(v);
+            v.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), QuizActivity.class);
+            intent.putExtra(EXTRA_TOPIC, getAdapterPosition());
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/edu/washington/gjdevera/quizdroid/QuizActivity.java
+++ app/src/main/java/edu/washington/gjdevera/quizdroid/QuizActivity.java
@@ -30,6 +30,8 @@ public class QuizActivity extends Activity implements TopicOverviewFragment.OnFr
         changeFragment(TopicOverviewFragment.newInstance(topicNumber)); // display topic overview
         final Topic topic = ((QuizApp) getApplication()).getRepository().getAllTopics().get(topicNumber);
         headerText.setText(topic.getTitle());
+        headerText.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_library_books, 0, 0, 0);
+        headerText.setCompoundDrawablePadding(12);
 
         final Button button = (Button) findViewById(R.id.btn_main);
         button.setOnClickListener(new View.OnClickListener() {
