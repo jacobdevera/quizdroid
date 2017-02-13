5e560c0919631df4387244001a954bd03789c6ee
Jacob Devera <jacob.devera12@gmail.com>
2017-02-13 13:42:24 -0800

    Added Preferences action to app bar

--- app/src/main/java/edu/washington/gjdevera/quizdroid/MainActivity.java
+++ app/src/main/java/edu/washington/gjdevera/quizdroid/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -57,6 +59,11 @@ public class MainActivity extends AppCompatActivity {
                 return topics.size();
             }
         });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 
     private class TopicViewHolder
